bff8214522e9ed2bac587f7a60406bdb73b66ffb
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2021-11-10 09:59:59 +0100

    Implemented example temperature and ppm chart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,7 +7,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def dashboard():
-    return render_template("index.html")
+    data = [{"year": year, "value": random.randint(80, 100) / 100 * (year - 1900)} for year in range(1900, 2022)]
+    temperatures = [random.randint(0, 15) for _ in range(1900, 2022)]
+    return render_template("index.html", data=data, temperatures=temperatures)
 
 
 if __name__ == "__main__":
