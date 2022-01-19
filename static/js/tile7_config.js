f6ad13bc52bd4b5d9da7b1319ab9b3ca4b85f7d3
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2022-01-19 11:43:27 +0100

    Fixed t7 labels

--- static/js/tile7_config.js
+++ static/js/tile7_config.js
@@ -7,13 +7,14 @@ const t7_route_offset = 30;
 
 const t7_route_total_height = t7_route_space + t7_route_height + t7_route_offset + t7_route_upper_padding * 2;
 
+const t7_unit_height = 40;
 const t7_chart_height = 230;
 const t7_bar_gap = 30;
 const t7_bar_text_space = 8;
 const t7_icon_size = 21;
 const t7_icon_space = 10;
-const t7_legend_height = 120;
+const t7_legend_height = 110;
 
-const t7_chart_total_height = t7_chart_height + t7_icon_size + t7_icon_space + t7_legend_height;
+const t7_chart_total_height = t7_unit_height + t7_chart_height + t7_icon_size + 2 * t7_icon_space + t7_legend_height;
 
 const t7_min_height = t7_route_total_height + t7_chart_total_height;
