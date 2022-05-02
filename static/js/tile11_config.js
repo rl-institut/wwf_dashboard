6b84fba7df98e7a2d8ee9f0b718bbc3db12d0292
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2022-05-02 16:48:29 +0200

    Implemented tile 11 (without flags yet)

--- static/js/tile11_config.js
+++ static/js/tile11_config.js
@@ -15,14 +15,18 @@ const t11_chart_axes_width = 40;
 const t11_chart_xaxis_height = 40;
 const t11_chart_width = width - 2 * t11_chart_axes_width;
 const t11_chart_height = 260;
-const t11_chart_sector_space = 10;
+const t11_chart_import_type_space = 10;
 const t11_chart_total_height = t11_chart_unit_height + t11_chart_height + t11_chart_xaxis_height;
 
-const t11_bar_color_reduction = "#008A88";
-const t11_bar_height = 40;
+const t11_bar_offset = 20;
 const t11_bar_title_height = 48;
+const t11_bar_title_offset = 15;
+const t11_bar_height = 25;
+const t11_bar_top_vspace = 40;
 const t11_bar_vspace = 15;
-const t11_bar_legend_size = 16;
-const t11_bar_total_height = t11_bar_title_height + 4 * t11_bar_vspace + t11_bar_height + t11_bar_legend_size;
+const t11_bar_text_width = 100;
+const t11_bar_flag_size = 20;
+const t11_bar_text_offset = 10;
+const t11_bar_total_height = t11_bar_offset + t11_bar_title_height + t11_bar_title_offset + t11_bar_top_vspace + 3 * t11_bar_vspace + 5 * t11_bar_height;
 
 const t11_min_height = t11_bar_total_height + t11_icon_total_height + t11_chart_total_height;
