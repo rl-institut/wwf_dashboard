cec06f3e8a0fa75753473fc3c2a72cdfdeeb6341
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-08-15 13:09:59 +0200

    Implement SMARD API scraping

--- scrape.py
+++ scrape.py
@@ -4,14 +4,17 @@ import json
 import js2xml
 import requests
 import pandas
+import time
+from collections import defaultdict
 
 RAW_DATA_FOLDER = "agora_data"
 AGORA_DATA_URL = "https://www.agora-energiewende.de/service/agorameter/chart/data/power_generation/{date}/{date}/today/chart.json"
+SMARD_DATA_URL = "https://www.smard.de/app/chart_data/{technology_id}/DE/{technology_id}_DE_hour_{date}.json"
 
 MAX_DATA_VALUE = 120000
 
 
-def get_agora_data_for_day(date: dt.date):
+def get_agora_data_for_day(date: dt.date) -> tuple[dict, float]:
     if date == dt.date.today():
         suffix = f"{date.isoformat()}_{dt.datetime.now().hour}.json"
     else:
@@ -74,3 +77,81 @@ def get_agora_data_for_day(date: dt.date):
         return df.to_dict(orient="records"), share["renewable_share"]
     except:
         return None, None
+
+
+def get_start_of_week(date: dt.date):
+    return dt.date(year=date.year, month=date.month, day=date.day - date.weekday())
+
+
+def get_smard_data_for_day(date: dt.date) -> tuple[dict, float]:
+    """Return data and renewable share for given date"""
+    technology_ids = {
+        "wind_onshore": 4067,
+        "pv": 4068,
+        "lignite": 1223,
+        "hard_coal": 4069,
+        "gas": 4071,
+        "fossils": 1227,
+        "biomass": 4066,
+        "hydro": 1226,
+        "wind_offshore": 1225,
+        "renewables": 1228,
+        "pump_storages": 4070,
+    }
+    week_date = get_start_of_week(date)
+    timestamp = time.mktime(
+        dt.datetime(week_date.year, week_date.month, week_date.day).timetuple()
+    )
+    timestamp_formatted = str(int(timestamp)) + "000"
+
+    week_day_index = 24 * date.weekday()
+
+    data = defaultdict(list)
+    for technology, technology_id in technology_ids.items():
+        url = SMARD_DATA_URL.format(
+            technology_id=technology_id, date=timestamp_formatted
+        )
+        response = requests.get(url)
+        technology_data = response.json()
+        for i in range(week_day_index, week_day_index + 24):
+            value = technology_data["series"][i][1]
+            if value is None:
+                break
+            data[technology].append(value)
+
+    production = {
+        "pv": data["pv"],
+        "wind_onshore": data["wind_onshore"],
+        "fossil": [
+            sum(z)
+            for z in zip(*[data[i] for i in ("lignite", "hard_coal", "gas", "fossils")])
+        ],
+        "renewables": [
+            sum(z)
+            for z in zip(
+                *[
+                    data[i]
+                    for i in (
+                        "biomass",
+                        "hydro",
+                        "wind_offshore",
+                        "renewables",
+                        "pump_storages",
+                    )
+                ]
+            )
+        ],
+    }
+    min_series_length = min(len(series) for series in production.values())
+    result_data = []
+    for i in range(min_series_length):
+        hourly_data = {key: value[i] * 1e-3 for key, value in production.items()}
+        hourly_data["index"] = i
+        result_data.append(hourly_data)
+
+    renewables_sum = sum(
+        sum(production[tech]) for tech in ("pv", "wind_onshore", "renewables")
+    )
+    share = renewables_sum / (renewables_sum + sum(production["fossil"])) * 100
+
+    return result_data, share
