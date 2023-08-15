029e38736b536617077b2ecfa48eeae0d34aa28c
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-08-15 13:09:42 +0200

    Add tests for new api

---
+++ tests/test_new_api.py
@@ -0,0 +1,25 @@
+
+import datetime as dt
+import scrape
+
+
+def test_agora_data():
+    date = dt.date(2023, 8, 14)
+    agora_data = scrape.get_agora_data_for_day(date)
+    assert len(agora_data[0]) == 24
+
+
+def test_start_of_week():
+    assert scrape.get_start_of_week(dt.date(2023, 8, 14)) == dt.date(2023, 8, 14)
+    assert scrape.get_start_of_week(dt.date(2023, 8, 15)) == dt.date(2023, 8, 14)
+    assert scrape.get_start_of_week(dt.date(2023, 8, 16)) == dt.date(2023, 8, 14)
+    assert scrape.get_start_of_week(dt.date(2023, 8, 17)) == dt.date(2023, 8, 14)
+    assert scrape.get_start_of_week(dt.date(2023, 8, 18)) == dt.date(2023, 8, 14)
+    assert scrape.get_start_of_week(dt.date(2023, 8, 19)) == dt.date(2023, 8, 14)
+    assert scrape.get_start_of_week(dt.date(2023, 8, 20)) == dt.date(2023, 8, 14)
+
+
+def test_smard_data():
+    date = dt.date(2023, 8, 14)
+    smard_data = scrape.get_smard_data_for_day(date)
+    assert len(smard_data[0]) == 24
