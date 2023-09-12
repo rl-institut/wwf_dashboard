65cf1c17c86c8240050a957e6b9d012e30e5f72b
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-09-12 17:28:07 +0200

    Fix tile 2 icons and pie height

--- static/js/tile2.js
+++ static/js/tile2.js
@@ -16,7 +16,7 @@ document.addEventListener("globalSetupComplete", function (e) {
   const t2_icon_offset_ideal = 30;
   let t2_icon_offset = -20;
   const t2_icons_rect_height = 26;
-  const t2_icon_size = 16;
+  const t2_icon_size = 20;
   const t2_icon_vspace = 5;
   const t2_icon_hspace = 14;
   const t2_icon_text_height = 20;
@@ -28,8 +28,8 @@ document.addEventListener("globalSetupComplete", function (e) {
   const t2_arrow_offset_ideal = 30;
   let t2_arrow_offset = 0;
   const t2_arrow_width = 50;
-  const t2_arrow_height = 65;
-  const t2_arrow_height_measured = 40;
+  const t2_arrow_height = 80;
+  const t2_arrow_height_measured = 20;
   const t2_arrow_text_height = 24;
   let t2_arrow_total_height = t2_arrow_offset + t2_arrow_height_measured;
 
@@ -44,7 +44,8 @@ document.addEventListener("globalSetupComplete", function (e) {
   let t2_pie_legend_padding_top = 0;
   const t2_pie_legend_hspace = 10;
   const t2_pie_legend_width = width - 2 * (t2_pie_hspace + t2_pie_radius);
-  let t2_pie_total_height = t2_pie_offset + t2_pie_icon_size + 2 * t2_pie_vspace + 2 * t2_pie_radius + t2_pie_legend_padding_top + t2_pie_legend_size;
+  const t2_pie_bottom = 10;
+  let t2_pie_total_height = t2_pie_offset + t2_pie_icon_size + 2 * t2_pie_vspace + 2 * t2_pie_radius + t2_pie_legend_padding_top + t2_pie_legend_size + t2_pie_bottom;
 
   const t2_min_height = t2_bar_total_height + t2_icon_total_height + t2_arrow_total_height + t2_pie_total_height;
   if (debug) {console.log("Puffer #2 min height = ", t2_min_height);}
