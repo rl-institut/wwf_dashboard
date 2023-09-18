3b6dfd2c407bc55e128accd878fa44c1b90e54d1
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-09-18 16:34:08 +0200

    Fix tile2 bottom

--- static/js/tile2.js
+++ static/js/tile2.js
@@ -29,7 +29,7 @@ document.addEventListener("globalSetupComplete", function (e) {
   let t2_arrow_offset = 0;
   const t2_arrow_width = 50;
   const t2_arrow_height = 80;
-  const t2_arrow_height_measured = 20;
+  const t2_arrow_height_measured = 80;
   const t2_arrow_text_height = 24;
   let t2_arrow_total_height = t2_arrow_offset + t2_arrow_height_measured;
 
@@ -44,7 +44,7 @@ document.addEventListener("globalSetupComplete", function (e) {
   let t2_pie_legend_padding_top = 0;
   const t2_pie_legend_hspace = 10;
   const t2_pie_legend_width = width - 2 * (t2_pie_hspace + t2_pie_radius);
-  const t2_pie_bottom = 10;
+  const t2_pie_bottom = 20;  // Quick fix, as pie area exceeds margin for some reason I don't understand
   let t2_pie_total_height = t2_pie_offset + t2_pie_icon_size + 2 * t2_pie_vspace + 2 * t2_pie_radius + t2_pie_legend_padding_top + t2_pie_legend_size + t2_pie_bottom;
 
   const t2_min_height = t2_bar_total_height + t2_icon_total_height + t2_arrow_total_height + t2_pie_total_height;
